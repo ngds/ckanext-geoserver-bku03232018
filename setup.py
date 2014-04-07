7513a2eb3702b07246c91e7917946e616dc5417e
asonnenschein <adrian.sonnenschein@gmail.com>
2014-04-07 14:12:54 -0700

    skeleton ckan extension

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(
+    name='ckanext-geoserver',
+    version=version,
+    description="API for adding Geoserver compatibility and functionality to CKAN.",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Arizona Geological Survey',
+    author_email='adrian.sonnenschein@azgs.az.gov',
+    url='http://www.geothermaldata.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.geoserver'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.geoserver.plugin:PluginClass
+    ''',
+)
