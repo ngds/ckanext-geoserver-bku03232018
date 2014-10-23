0a248aafdb4db86b9f00458683a8548f487c78af
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-23 13:42:04 -0700

    jquery onClick

--- ckanext/geoserver/fanstatic/geoserver_resource_form.js
+++ ckanext/geoserver/fanstatic/geoserver_resource_form.js
@@ -7,7 +7,12 @@ ckan.module('geoserver_resource_form', function ($, _) {
       this.el.on('click', this._onClick);
     },
     _onClick: function () {
-      console.log($(this).attr('name'));
+      var target = $(this.el);
+      if (target.hasClass('active')) {
+        target.removeClass('active');
+      } else {
+        target.addClass('active');
+      }
     }
   }
 });
