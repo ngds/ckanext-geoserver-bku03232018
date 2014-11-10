36dccca692fd42e9f0df173011e793c1c840a5cf
asonnenschein <adrian.sonnenschein@gmail.com>
2014-11-10 16:03:28 -0700

    geoserver publish template controller

--- ckanext/geoserver/fanstatic/geoserver_publish_ogc.js
+++ ckanext/geoserver/fanstatic/geoserver_publish_ogc.js
@@ -9,6 +9,7 @@ ckan.module('geoserver_publish_ogc', function ($, _) {
         ;
 
       obj = this;
+      obj.fieldnames = '';
 
       $.proxyAll(this, /_on/);
       this.el.on('click', this._onClick);
@@ -24,8 +25,9 @@ ckan.module('geoserver_publish_ogc', function ($, _) {
 
       obj.postSearch(id, function (res) {
         if (res.success) {
-          fields = obj.parseResponse(res);
-          console.log(fields);
+          obj.fieldnames = obj.parseResponse(res);
+          obj.sandbox.client.getTemplate('geoserver_publish_ogc_form.html',
+            obj.options, obj._onReceiveSnippet);
         }
         else {
           return null;
@@ -33,6 +35,33 @@ ckan.module('geoserver_publish_ogc', function ($, _) {
       });
 
       return false;
+    },
+    _onReceiveSnippet: function (html) {
+      var obj
+        , latSelect
+        , lngSelect
+        , fields
+        , option
+        , i
+        ;
+
+      obj = this;
+      fields = obj.fieldnames;
+
+      $(html).modal('show');
+
+      latSelect = $(html).find('#geoserver_lat_field');
+      lngSelect = $(html).find('#geoserver_lng_field');
+
+      for (i = 0; i < fields.length; i++) {
+        option = $('<option></option>')
+          .attr('value', fields[i])
+          .text(fields[i])
+        ;
+
+        latSelect.append(option);
+        lngSelect.append(option);
+      }
     },
     postSearch: function (id, callback) {
       var path
